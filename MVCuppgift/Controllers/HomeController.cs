05887a2149e530ab4050b231c3b2b68ea6c2b1c1
Lexicon <Lexicon@DESKTOP-079NTLE>
2022-10-31 15:55:23 +0100

    Add project files.

---
+++ MVCuppgift/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCuppgift.Models;
+using System.Diagnostics;
+
+namespace MVCuppgift.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            List<ProjectModel> projectList = new List<ProjectModel>();
+            projectList.Add(new ProjectModel("Sokoban", "Move the boxes to win", "https://github.com/DevSeif/Sokoban-game"));
+            projectList.Add(new ProjectModel("Vending machine", "Buy products from a vending machine", "https://github.com/DevSeif/Vending-Machine"));
+            projectList.Add(new ProjectModel("Website", "Simple website with a login page", "https://github.com/DevSeif/htmluppgift2"));
+
+            return View(projectList);
+        }
+
+
+    }
+}
---
+++ MVCuppgift/Models/ProjectModel.cs
@@ -0,0 +1,16 @@
+﻿namespace MVCuppgift.Models
+{
+    public class ProjectModel
+    {
+        public string ProjectName { get; set; }
+        public string ProjectDescription { get; set; }
+        public string ProjectLink { get; set; }
+
+        public ProjectModel(string projectName, string projectDescription, string projectLink)
+        {
+            ProjectName = projectName;
+            ProjectDescription = projectDescription;
+            ProjectLink = projectLink;
+        }
+    }
+}
